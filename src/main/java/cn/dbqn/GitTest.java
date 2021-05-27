5494ce2c97a3f4fb998114e43a2c98c39d8cfe80
root <2510216122@qq.com>
2021-05-27 15:31:59 +0800

    second commit

--- src/main/java/cn/dbqn/GitTest.java
+++ src/main/java/cn/dbqn/GitTest.java
@@ -3,5 +3,8 @@ package cn.dbqn;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.Git");
+        System.out.println("hello.Git2");
+
+
     }
 }
